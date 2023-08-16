7886941774af2bf4353a6c8945a6076075a7236e
cagomni <cagomni@oocl.com>
2023-08-16 17:27:03 +0800

    feat: implement should_return_message_with_name_and_age_when_introduce_given_person

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -2,4 +2,16 @@ package ooss;
 
 public class Person {
 
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age
+    ) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.",name,age);
+    }
 }
