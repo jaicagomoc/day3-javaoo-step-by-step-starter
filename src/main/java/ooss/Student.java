986f6b8b49264bfb68fd4f10bd3ef502e124ebb9
cagomni <cagomni@oocl.com>
2023-08-16 21:04:34 +0800

    feat: implemented should_return_message_with_name_and_age_when_introduce_given_student as well as all the step2test

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -5,4 +5,9 @@ public class Student extends Person {
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a student.";
+    }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -6,7 +6,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
-    public Teacher(int id, String name, int age) {
+    public Teacher(int id, String name, int age ) {
         super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+ " I am a teacher.";
     }
 }
